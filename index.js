9faf516075d5118ed4aa94881dd7aaa36d85dcea
Paolo D'Alessandro <paolodalex96@gmail.com>
2022-05-30 11:34:56 +0200

    fix: now mobil_nav doesn't disappear immediately

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const mobile_nav_exit = document.getElementById('mobile-nav_options_container-ex
 const mobile_nav_options_container = document.getElementById("mobile-nav_options_container");
 mobile_nav.addEventListener("click", function() {
     mobile_nav_options_container.style.transform = "translateX(0)";
-    mobile_nav.style.display = "none";
+    mobile_nav.style.animation = "none";
+    mobile_nav.style.animation = "rotate .4s ease-in";
+    setTimeout(function() {
+        mobile_nav.style.display = "none";
+    }, 500);
 });
 
 mobile_nav_exit.addEventListener("click", function() {
