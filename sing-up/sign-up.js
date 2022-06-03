0c514652a08afac8edf1f0417d44bb865543fd60
Paolo D'Alessandro <paolodalex96@gmail.com>
2022-06-03 16:56:11 +0200

    feat: improved and fixed sign-in log-in

---
+++ sing-up/sign-up.js
@@ -0,0 +1,12 @@
+var mobile_signIn_button = document.getElementById("mobile_sign-in_next");
+
+console.log(mobile_signIn_button);
+
+mobile_signIn_button.addEventListener("click", nextStep());
+
+nextStep();
+
+function nextStep() {
+    console.log("ciao");
+}
+
